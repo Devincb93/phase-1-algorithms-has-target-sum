6ccdb1faa0460f4bd53b2bfd9e46f7ca91de5060
Devin <devincb93@gmail.com>
2023-08-21 17:44:06 -0700

    Target Sum Lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i =0; i < array.length; i++){
+    const added = target - array[i]
+    for (let j = i +1; j < array.length; j++) {
+      if (array[j] === added) return true;
+    } 
+  }
+ return false;
 }
 
 /* 
@@ -8,6 +15,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  hasTargetSum([3, 8, 12, 4, 11, 7], 10))
+  go through each number in the array, starting with the first number
+  and add each following number that you go through to see if it equals the target
+  value. 
+  If the first number doesn't add up with any other numbers for the target value,
+  move on to the next number and repeat the process excluding the first number.
+  If the numbers iterated through equal the target value,
+  return true
+  If at the end of the array and no numbers add up to the target value 
+  return false
+
+
 */
 
 /*
@@ -29,6 +48,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
